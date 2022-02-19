c7f7991d0e5e3f7dc2c50e064982a1b6d590d64d
Linda Martinez <lindanmjw@gmail.com>
2022-02-18 22:10:34 -0500

    adding about and trying to figure out resume

---
+++ src/components/about.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Linda from '../assets/linda.jpg';
+
+function About() {
+  return (
+    <section className="title">
+      <h1 class="name">about: Linda Martinez</h1>
+      <hr></hr>
+
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+          <img className='avatar' src={Linda}  />
+
+          <p>After years in the life insurance industry I was given the assignment to help create a new system for our
+            new applications and at that moment I was hooked. Every time I had the opportunity I took advantage of any
+            chance I could to learn more and more about systems makeup, coding and testing.</p>
+
+          <p>My specialties include quickly learning new skills, technical writing, problem solving, programing languages
+            such as Computer Science fundamentals, Javascript, ReactJS, Redux, VueJS, Vuex, NodeJS, MongoDB, CSS,
+            HTML and other technologies. I am still eagerly learning additional
+            languages to broaden my coding abilities.</p>
+          <p>
+            View full <a href="#/resume" class="link">resume</a>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About;
